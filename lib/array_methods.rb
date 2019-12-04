27614e20dd7ca973a604aa13652bdb6f92867704
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-04 18:29:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,7 +11,12 @@
 # end
 
 def find_element_index(array, value_to_find)
-
+  array.length.times do |index|
+    if array.length == index 
+      return nil 
+    elsif array[index] == value_to_find
+    return array[index]
+  end
 end
 
 def find_max_value(array)
